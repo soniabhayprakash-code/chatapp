a43e4f57dba386c27dc7d3562fb5cb733776a3fd
Abhay <soniabhayprakash@gmail.com>
2026-01-11 12:32:45 +0530

    Delete isVerified.js

--- isVerified.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-  if (!req.user || !req.user.isVerified) {
-    return res.status(403).json({
-      success: false,
-      message: "OTP verification required."
-    });
-  }
-  next();
-};
