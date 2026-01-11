553093a8bc42634bce10a17a6ef905459e2b4099
Abhay <soniabhayprakash@gmail.com>
2026-01-11 14:56:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,12 +55,13 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(3000, () => {
+http.listen(PORT, () => {
   console.log('=====================================');
   console.log('--Started--');
-  console.log('Go to Browser: http://localhost:3000');
+  console.log('Go to Browser: http://localhost:PORT');
   console.log('=====================================');
 });
+
 
 
 
