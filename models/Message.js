763229dd21693f72ac274a63bab200aaf0facb88
Abhay <soniabhayprakash@gmail.com>
2026-01-24 11:23:19 +0530

    Add files via upload

---
+++ models/Message.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  roomId: {
+    type: String,
+    required: true
+  },
+  sender: {
+    type: String,
+    required: true
+  },
+  receiver: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Message", messageSchema);
