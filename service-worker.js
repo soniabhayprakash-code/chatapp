e08e05ffeb468238cb692b2e654c0a67cc723f1b
Abhay <soniabhayprakash@gmail.com>
2026-02-05 19:47:22 +0530

    Refactor service worker code structure

--- service-worker.js
+++ service-worker.js
@@ -52,25 +52,8 @@ self.addEventListener("fetch", event => {
 
 });
 
-// self.addEventListener("push", event => {
-//   const data = event.data.json();
 
-//   self.registration.showNotification(data.title, {
-//     body: data.body,
-//     icon: "/icon-192.png",
-//     badge: "/icon1-192.png"
-//   });
-// });
 
-// self.addEventListener("notificationclick", event => {
-//   event.notification.close();
-
-//   const url = event.notification.data?.url || "/";
-
-//   event.waitUntil(
-//     clients.openWindow(url)
-//   );
-// });
 
 
 
