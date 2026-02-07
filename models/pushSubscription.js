e817fbb5494866ab3b828867d11011213b714b22
Abhay <soniabhayprakash@gmail.com>
2026-02-07 17:57:08 +0530

    Delete models/pushSubscription.js

--- models/pushSubscription.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const pushSchema = new mongoose.Schema({
-  endpoint: String,
-  keys: Object,
-  mobile: String
-});
-
-module.exports = mongoose.model("PushSubscription", pushSchema);
